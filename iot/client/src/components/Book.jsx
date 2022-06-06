1bc34d8836c1a8aae4977762fb721e27beed3a49
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-06 22:41:32 +0900

    implement book

--- iot/client/src/components/Book.jsx
+++ iot/client/src/components/Book.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Book = () => {
-  return <div className='object-container'>Book</div>;
+const Book = ({ lastObject }) => {
+  const [turnedOn, setTurnedOn] = useState(false);
+
+  useEffect(() => {
+    if (lastObject.object === 'book') setTurnedOn(!turnedOn);
+  }, [lastObject]);
+
+  return <Wrapper>{turnedOn ? <Img src='phone-on.jpg'></Img> : <Img src='phone-off.jpg'></Img>}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 export default Book;
