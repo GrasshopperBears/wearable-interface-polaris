eb0635a6463bbd7751c4efb84119be54604c6fc0
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-06 22:17:22 +0900

    implement kettle

--- iot/client/src/components/Kettle.jsx
+++ iot/client/src/components/Kettle.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Kettle = () => {
-  return <div className='object-container'>Kettle</div>;
+const Kettle = ({ lastObject }) => {
+  const [turnedOn, setTurnedOn] = useState(false);
+
+  useEffect(() => {
+    if (lastObject.object === 'kettle') setTurnedOn(!turnedOn);
+  }, [lastObject]);
+
+  return <Wrapper>{turnedOn ? <Img src='kettle-on.jpg'></Img> : <Img src='kettle-off.jpg'></Img>}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 export default Kettle;
