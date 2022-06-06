06f615f02a52775aab41acd1869de79bfaab193f
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-06 21:45:32 +0900

    add title of object

--- iot/client/src/components/ObjectGrid.jsx
+++ iot/client/src/components/ObjectGrid.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ObjectGrid = ({ children, object, lastObject }) => {
-  return <Wrapper selected={object === lastObject}>{children}</Wrapper>;
+  return (
+    <Wrapper selected={object === lastObject}>
+      <Title>{object}</Title>
+      {children}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
@@ -10,6 +15,19 @@ const Wrapper = styled.div`
   width: 33vw;
   height: 50vh;
   border: ${({ selected }) => (selected ? '8px solid #FDCA40' : '')};
+  position: relative;
+`;
+
+const Title = styled.div`
+  background-color: black;
+  color: white;
+  font-size: 3rem;
+  border-radius: 7px;
+  text-align: center;
+  width: 24rem;
+  padding: 10px 0;
+  position: absolute;
+  left: calc(50% - 12rem);
 `;
 
 export default ObjectGrid;
