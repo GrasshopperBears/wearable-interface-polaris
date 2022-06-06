94b5a15636093dcc9944eb97bd364b98d5f1bd66
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-07 00:04:31 +0900

    implement desk

--- iot/client/src/components/Desk.jsx
+++ iot/client/src/components/Desk.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Desk = () => {
-  return <div className='object-container'>Desk</div>;
+const Desk = ({ lastObject }) => {
+  const [audio] = useState(new Audio('music.m4a'));
+  const [turnedOn, setTurnedOn] = useState(false);
+
+  useEffect(() => {
+    audio.autoplay = false;
+    if (lastObject.object === 'desk') {
+      if (!turnedOn) audio.play();
+      setTurnedOn(!turnedOn);
+    }
+  }, [lastObject]);
+
+  return <Wrapper>{turnedOn ? <Img src='speaker-on.jpg'></Img> : <Img src='speaker-off.jpg'></Img>}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 export default Desk;
