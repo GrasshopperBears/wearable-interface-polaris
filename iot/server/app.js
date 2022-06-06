dfe7fc00cb2078732a29d33b0e2a069448145fe3
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-06 21:38:14 +0900

    emit object name when api called

--- iot/server/app.js
+++ iot/server/app.js
@@ -5,28 +5,34 @@ const cors = require('@koa/cors');
 const logger = require('koa-logger');
 const { Server } = require('socket.io');
 
-const PORT = 4000;
-
 const app = new Koa();
-const server = createServer(app.callback);
+const server = createServer(app.callback());
 const io = new Server(server, { cors: { origin: 'http://localhost:3000' } });
 const router = new Router();
+
+const PORT = 4000;
+let client = undefined;
 
 app.use(logger());
 app.use(cors());
 
-router.post('/detect/:object', async (ctx, next) => {
+router.post('/detect/:object', async (ctx) => {
   const { object } = ctx.params;
+
+  if (client !== undefined) client.emit('detect', object);
+
   ctx.status = 200;
 });
 
-app.use(router.routes());
-app.use(async (ctx) => {
+router.all('/', async (ctx) => {
   ctx.status = 404;
 });
 
+app.use(router.routes()).use(router.allowedMethods());
+
 io.on('connection', (socket) => {
   console.log(`Client connected: id=${socket.id}`);
+  client = socket;
 });
 
 server.listen(PORT, () => {
