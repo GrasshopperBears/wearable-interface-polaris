dcd1230c19a968ac465d7bb3e15afd40cea24cb0
Jinwoo Lee <starday456@kaist.ac.kr>
2022-06-06 22:45:08 +0900

    implement microwave

--- iot/client/src/components/Microwave.jsx
+++ iot/client/src/components/Microwave.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Microwave = () => {
-  return <div className='object-container'>Microwave</div>;
+const Microwave = ({ lastObject }) => {
+  const [turnedOn, setTurnedOn] = useState(false);
+
+  useEffect(() => {
+    if (lastObject.object === 'microwave') setTurnedOn(!turnedOn);
+  }, [lastObject]);
+
+  return <Wrapper>{turnedOn ? <Img src='microwave-on.jpg'></Img> : <Img src='microwave-off.jpg'></Img>}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 export default Microwave;
